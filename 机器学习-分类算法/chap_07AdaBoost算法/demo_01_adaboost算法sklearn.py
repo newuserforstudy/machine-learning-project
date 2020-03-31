afef2d493ea52b3d913c0eb1a8fe01084da53059
newuserforstudy <33213129+newuserforstudy@users.noreply.github.com>
2020-03-31 15:53:16 +0800

    Add files via upload

---
+++ 机器学习-分类算法/chap_07AdaBoost算法/demo_01_adaboost算法sklearn.py
@@ -0,0 +1,36 @@
+# *_* coding:utf-8 *_*
+# @author:sdh
+# @Time : 2020/3/29 0029 7:50
+
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier, RandomForestClassifier
+
+
+def create_data():
+    iris = load_iris()
+    df = pd.DataFrame(iris.data, columns=iris.feature_names)
+    df['label'] = iris.target
+    df.columns = ['sepal length', 'sepal width', 'petal length', 'petal width', 'label']
+    data = np.array(df.iloc[:100, [0, 1, -1]])
+    for i in range(len(data)):
+        if data[i, -1] == 0:
+            data[i, -1] = -1
+    # print(data)
+    return data[:, :2], data[:, -1]
+
+
+X, y = create_data()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+plt.scatter(X[:50, 0], X[:50, 1], label='0')
+plt.scatter(X[50:, 0], X[50:, 1], label='1')
+plt.legend()
+plt.show()
+
+clf = AdaBoostClassifier(n_estimators=100, learning_rate=0.5)
+clf.fit(X_train, y_train)
+s = clf.score(X_test, y_test)
+print(s)
